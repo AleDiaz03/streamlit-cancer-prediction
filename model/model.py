b18df959e996b21a74b937b414c73455731491eb
Alejandro Diaz Tamayo <aletamayo702@gmail.com>
2024-03-24 08:29:36 +0100

    Fixing a few bugs and adding pipreqs

---
+++ model/model.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, classification_report
+import pickle
+
+data = pd.read_csv("../data/data.csv")
+data = data.drop(['Unnamed: 32', 'id'], axis=1)
+data['diagnosis'] = data['diagnosis'].map({'M':1, 'B':0})
+print(f"Total missing values: {data.isna().sum().sum()}")
+X = data.drop(['diagnosis'], axis=1)
+y = data['diagnosis']
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+X_train, X_test, y_train, y_test = train_test_split(X, 
+                                                    y, 
+                                                    test_size=0.2, 
+                                                    random_state=42)
+model = LogisticRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+print(f'Accuracy of the model: {accuracy_score(y_test, y_pred)}')
+print(f'Precision of the model: {precision_score(y_test, y_pred)}')
+print(f'Recall of the model: {recall_score(y_test, y_pred)}')
+print(f'Classification report of the model \n: {classification_report(y_test, y_pred)}')
+
+with open('model.pkl', 'wb') as f:
+    pickle.dump(model, f)
+with open('scaler.pkl', 'wb') as f:
+    pickle.dump(scaler, f)
